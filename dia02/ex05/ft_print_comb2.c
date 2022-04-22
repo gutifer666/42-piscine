cf250008157ea5fd8ad6f45b6d97ab72c09bd855
gutifer666 <gutifer666@gmail.com>
2022-04-23 01:18:03 +0200

    upload valid function

--- dia02/ex05/ft_print_comb2.c
+++ dia02/ex05/ft_print_comb2.c
@@ -1,51 +1,27 @@
 void	ft_putchar(char c);
 
-void	ft_print(char a, char b, char c, char d)
-{
-	ft_putchar(a);
-	ft_putchar(b);
-	ft_putchar(' ');
-	ft_putchar(c);
-	ft_putchar(d);
-	if (!((a == '9') && (b == '8') && (c == '9') && (d == '9')))
-	{
-		ft_putchar(',');
-		ft_putchar(' ');
-	}
-}
-
 void	ft_print_comb2(void)
 {
-	char	a;
-	char	b;
-	char	c;
-	char	d;
+	int	x[2];
 
-	a = '0';
-	b = '0';
-	c = '0';
-	d = '0';
-	while (a <= '9')
+	x[0] = 0;
+	while (x[0] < 100)
 	{
-		b = '0';
-		while (b <= '9')
+		x[1] = x[0] + 1;
+		while (x[1] < 100)
 		{
-			c = '0';
-			while (c <= '9')
+			ft_putchar(x[0] / 10 + '0');
+			ft_putchar(x[0] % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(x[1] / 10 + '0');
+			ft_putchar(x[1] % 10 + '0');
+			if (!((x[0] == 98) && (x[1] == 99)))
 			{
-				d = '0';
-				while (d <= '9')
-				{
-					if ((a + b) < (c + d))
-					{
-						ft_print(a, b, c, d);
-					}
-					d++;
-				}
-				c++;
+				ft_putchar(',');
+				ft_putchar(' ');
 			}
-			b++;
+			x[1]++;
 		}
-		a++;
+		x[0]++;
 	}
 }
